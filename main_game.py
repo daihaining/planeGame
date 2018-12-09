b9b773e3892617d4513a7deb21ba01807ac26df9
cjj <3481087582@qq.com>
2018-12-09 22:40:29 +0800

    sceond commit

--- main_game.py
+++ main_game.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 from pygame.locals import *
-from plane import Bkground,Player
+from plane import Bkground,Player,Bullet
 
 
 SCREEN_WIDTH=450
@@ -19,19 +19,33 @@ clock=pygame.time.Clock()
 bk=Bkground()
 player=Player((200,500))
 
+bullets=pygame.sprite.Group()				#子弹组
+
+count=0
+
 while True:
 	
 	screen.fill((0,0,0))
 	bk.draw_bkimg(screen)				#绘制背景图片
-	screen.blit(player.image,player.rect)
+	player.draw(screen)
+	bullets.draw(screen)
 
 	for event in pygame.event.get():
 		if event.type==QUIT:
 			pygame.quit()
 			sys.exit()
+
+	if count%8==0:																#设置子弹发射频率
+		bullets.add(Bullet(player.rect.midtop))
+	
 	bk.move()
 	player.update(pygame.time.get_ticks())
 	player.move(pygame.key.get_pressed())
+	bullets.update()												#更新子弹位置
 
+	if len(bullets)>0 and bullets.sprites()[0].rect.bottom<0:
+		bullets.remove(bullets.sprites()[0])						#删除越界的子弹
+
+	count+=1
 	pygame.display.update()
 	clock.tick(20)
--- plane.py
+++ plane.py
@@ -22,17 +22,20 @@ class Bkground:
 			if(self.pos[i]<-HEIGHT):
 				self.pos[i]=HEIGHT
 
-#玩家飞机图片
-player_imgs=[]
-for i in range(1,3):
-	player_imgs.append(pygame.image.load('img/hero{}.png'.format(i)))
+
 #玩家类
 class Player(pygame.sprite.Sprite):
+	
+	#玩家飞机图片
+	player_imgs=[]
+	for i in range(1,3):
+		player_imgs.append(pygame.image.load('img/hero{}.png'.format(i)))		#加载所需要的图片文件
 	
 	#初始化玩家位置
 	def __init__(self,init_pos):
 		super().__init__()
-		self.image=player_imgs[0]
+
+		self.image=Player.player_imgs[0]
 		self.rect=self.image.get_rect()
 		self.rect.topleft=init_pos
 		self.num=0								#图片编号
@@ -42,7 +45,7 @@ class Player(pygame.sprite.Sprite):
 
 		if(current_time>self.update_time):
 			self.num=(self.num+1)%2
-			self.image=player_imgs[self.num]
+			self.image=Player.player_imgs[self.num]
 			self.update_time=current_time+60
 
 	def move(self,key_pressed):
@@ -55,6 +58,25 @@ class Player(pygame.sprite.Sprite):
 		if key_pressed[pygame.K_s] and self.rect.y<HEIGHT-self.rect.height:
 			self.rect.y+=10
 
+	def draw(self,screen):
+		screen.blit(self.image,self.rect)
+
+
+class Bullet(pygame.sprite.Sprite):
+	
+	#初始化子弹
+	def __init__(self,pos):
+		super().__init__()
+		self.image=pygame.image.load('img/bullet1.png').convert_alpha()
+		self.rect=self.image.get_rect()
+		self.speed=20												#子弹速度
+		self.rect.midbottom=pos 									#设置坐标
+
+
+	def update(self):
+		self.rect.y-=self.speed
+
+	
 
 
 
