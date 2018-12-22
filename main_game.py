0697fe22e4d36d93880e0b45fdd81a683abdd833
cjj <3481087582@qq.com>
2018-12-22 22:33:16 +0800

    Add new Function

--- main_game.py
+++ main_game.py
@@ -25,32 +25,43 @@ enemys=pygame.sprite.Group()
 
 count=0
 
-while True:
+while player.is_alive():
 	
 	screen.fill((0,0,0))
 	bk.draw_bkimg(screen)				#绘制背景图片
 	player.draw(screen)					#绘制玩家飞机
 	bullets.draw(screen)				#绘制子弹
-	enemys.draw(screen)
+	
+	for enemy in enemys:
+		enemy.draw(screen)
+
 
 	for event in pygame.event.get():
 		if event.type==QUIT:
 			pygame.quit()
 			sys.exit()
 
-	if count%8==0:																#设置子弹发射频率
-		bullets.add(Bullet(player.rect.midtop))
-		enemys.add(Enemy())
+	if count%8==0:
+		if player.num<2:										#设置子弹发射频率(飞机存活时)
+			bullets.add(Bullet(player.rect.midtop))
+		if len(enemys)<9:										#控制同时出现的敌机数量
+			enemys.add(Enemy())
 
 	
 	bk.move()
-	player.update(pygame.time.get_ticks())
+	player.update(pygame.time.get_ticks(),enemys)
 	player.move(pygame.key.get_pressed())
-	bullets.update()												#更新子弹位置
+	bullets.update()							#更新子弹位置
 	enemys.update(bullets)
 
 	#print(len(enemys))							#测试剩余多少子弹
 
-	count+=1
+	count+=1									#计数器控制频率
 	pygame.display.update()
-	clock.tick(20)
+	clock.tick(25)
+
+
+pygame.quit()
+print('游戏结束')
+sys.exit()
+
--- plane.py
+++ plane.py
@@ -27,9 +27,9 @@ class Bkground:
 class Player(pygame.sprite.Sprite):
 	
 	#玩家飞机图片
-	player_imgs=[]
-	for i in range(1,3):
-		player_imgs.append(pygame.image.load('img/hero{}.png'.format(i)))		#加载所需要的图片文件
+	player_imgs=['img/hero1.png','img/hero2.png','img/hero_blow1.png','img/hero_blow2.png','img/hero_blow3.png','img/hero_blow4.png']
+	for i in range(0,len(player_imgs)):
+		player_imgs[i]=pygame.image.load(player_imgs[i])		#加载所需要的图片文件
 	
 	#初始化玩家位置
 	def __init__(self,init_pos):
@@ -38,28 +38,50 @@ class Player(pygame.sprite.Sprite):
 		self.image=Player.player_imgs[0]
 		self.rect=self.image.get_rect()
 		self.rect.topleft=init_pos
-		self.num=0								#图片编号
+		self.num=0								#图片编号(同时记录状态)
 		self.update_time=0						#更新时间
+		self.radius=min(self.rect.size)/2*0.8	#设置半径属性(方便碰撞检测)
+		self.speed=10							#飞机移动时的速度
 
-	def update(self,current_time):
+	def update(self,current_time,enemys):
+
+		if self.num<2 and self.__is_collision(enemys):
+			self.num=2
 
 		if(current_time>self.update_time):
-			self.num=(self.num+1)%2
-			self.image=Player.player_imgs[self.num]
+			
+			if self.num<2:
+				self.num=(self.num+1)%2
+			else:
+				self.num+=1
+			if self.num<len(Player.player_imgs):
+				self.image=Player.player_imgs[self.num]
+
 			self.update_time=current_time+60
 
 	def move(self,key_pressed):
 		if key_pressed[pygame.K_a] and self.rect.x>0:
-			self.rect.x-=10
+			self.rect.x-=self.speed
 		if key_pressed[pygame.K_d] and self.rect.x<WIDTH-self.rect.width:
-			self.rect.x+=10
+			self.rect.x+=self.speed
 		if key_pressed[pygame.K_w] and self.rect.y>0:
-			self.rect.y-=10
+			self.rect.y-=self.speed
 		if key_pressed[pygame.K_s] and self.rect.y<HEIGHT-self.rect.height:
-			self.rect.y+=10
+			self.rect.y+=self.speed
 
 	def draw(self,screen):
 		screen.blit(self.image,self.rect)
+
+	#判断是否与敌机碰撞
+	def __is_collision(self,enemys):
+		for enemy in enemys:
+			if pygame.sprite.collide_circle(self,enemy):
+				return True		
+		return False
+
+	#返回玩家是否存活
+	def is_alive(self):
+		return self.num<len(Player.player_imgs)
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -78,12 +100,13 @@ class Bullet(pygame.sprite.Sprite):
 		if self.rect.bottom<=0:
 			self.kill()												#删除子弹(子弹越界时)
 
-
 #敌机类
 class Enemy(pygame.sprite.Sprite):
 	
 	enemy_imgs=[['img/enemy0.png','img/enemy0_down1.png','img/enemy0_down2.png','img/enemy0_down3.png','img/enemy0_down4.png'],
-				['img/enemy1.png'],['img/enemy2.png']];
+				['img/enemy1.png','img/enemy1_down1.png','img/enemy1_down2.png','img/enemy1_down3.png','img/enemy1_down4.png'],
+				['img/enemy2.png','img/enemy2_down1.png','img/enemy2_down2.png','img/enemy2_down3.png','img/enemy2_down4.png',
+				'img/enemy2_down5.png','img/enemy2_down6.png']];
 	for i in range(0,len(enemy_imgs)):
 		for j in range(0,len(enemy_imgs[i])):
 			enemy_imgs[i][j]=pygame.image.load(enemy_imgs[i][j])	#加载图片
@@ -91,17 +114,23 @@ class Enemy(pygame.sprite.Sprite):
 	#初始化敌机
 	def __init__(self):
 		super().__init__()
-		self.id=0						#图片下标编号
-		self.state=0					#状态(方便绘制不同图片)
+		self.id=Enemy.__init_enemy_id()						#不同飞机编号
+		self.state=0										#状态(方便绘制不同图片)
 		self.image=Enemy.enemy_imgs[self.id][self.state]
 		self.rect=self.image.get_rect()
-		self.speed=random.randint(4,9)										#速度
+		self.speed=random.randint(5,10)						#速度
 		self.rect.topleft=(random.uniform(0,WIDTH-self.rect.w),-self.rect.height) 	#设置坐标
+		self.radius=min(self.rect.size)/2*0.9					#设置半径(方便碰撞检测)
+		self.blood=self.id*2+1									#敌机血量(根据飞机类型不同血量不同)
+		self.sum_blood=self.blood 								#敌机总血量方便绘制血条
 
 	def update(self,bullets):
-		self.rect.y+=self.speed
 
-		if self.state>0 or (self.state==0 and self.__is_collision(bullets)):
+		if self.blood>0:						#敌机血量大于零时
+			self.rect.y+=self.speed
+			self.__is_collision(bullets)
+
+		if self.blood==0:
 			self.state+=1
 			if self.state<len(Enemy.enemy_imgs[self.id]):
 				self.image=Enemy.enemy_imgs[self.id][self.state]			#更新图片
@@ -112,13 +141,26 @@ class Enemy(pygame.sprite.Sprite):
 	#判断是否与子弹碰撞
 	def __is_collision(self,bullets):
 		for bullet in bullets:
-			if self.rect.colliderect(bullet.rect):
+			if pygame.sprite.collide_rect(self,bullet):
+				self.blood-=1										#敌机血量减少
 				bullet.kill()										#消灭敌机后子弹销毁
-				return True
 
-		return False
+	def draw(self,screen):
+		screen.blit(self.image,self.rect)
+		#绘制血条
+		if self.id>0:
+			scale=self.blood/self.sum_blood
+			pygame.draw.rect(screen,(0,255,0),(self.rect.x,self.rect.y-12,self.rect.w*scale,2))
+			pygame.draw.rect(screen,(255,0,0),(self.rect.x+self.rect.w*scale,self.rect.y-12,self.rect.w*(1-scale),2))
 
-	
 
-
-
+	#产生不同飞机
+	@staticmethod
+	def __init_enemy_id():
+		num=random.random()
+		if num<=0.6:
+			return 0
+		elif num>0.96:
+			return 2
+		else:
+			return 1
